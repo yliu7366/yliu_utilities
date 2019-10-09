5cef1e0bbd4368d0e2148e3822823751c0a667e7
Yanling Liu <30328162+yliu7366@users.noreply.github.com>
2019-10-09 13:41:12 -0400

    indentation formating

--- SimpleITK_label_statistics.py
+++ SimpleITK_label_statistics.py
@@ -1,24 +1,26 @@
+#example code for using SimpleITK label statistics analysis functions
+
 for image in files:
-        img = sitk.ReadImage(os.path.join(originalPath, image))
-        imgPredict = sitk.ReadImage(os.path.join(labelPath, os.path.splitext(image)[0] + '.png'))
+  img = sitk.ReadImage(os.path.join(originalPath, image))
+  imgPredict = sitk.ReadImage(os.path.join(labelPath, os.path.splitext(image)[0] + '.png'))
 
-        labels = sitk.RelabelComponent(sitk.ConnectedComponent(imgPredict))
-        labelsNpy = sitk.GetArrayFromImage(labels)
-        print(labelsNpy.shape, np.max(labelsNpy))
-        np.save(os.path.join(relabelPath, os.path.splitext(image)[0] + '.npy'), labelsNpy)
+  labels = sitk.RelabelComponent(sitk.ConnectedComponent(imgPredict))
+  labelsNpy = sitk.GetArrayFromImage(labels)
+  print(labelsNpy.shape, np.max(labelsNpy))
+  np.save(os.path.join(relabelPath, os.path.splitext(image)[0] + '.npy'), labelsNpy)
 
-        shapeLabelFilter = sitk.LabelShapeStatisticsImageFilter()
-        shapeLabelFilter.Execute(labels)
+  shapeLabelFilter = sitk.LabelShapeStatisticsImageFilter()
+  shapeLabelFilter.Execute(labels)
 
-        csvFile = os.path.splitext(image)[0] + '.csv'
-        with open(os.path.join(csvPath, csvFile), 'w', newline='') as csvfile:
-            writer = csv.writer(csvfile)
-            writer.writerow(['id', 'roundness', 'pixels', 'bboxX', 'bboxY', 'bboxWidth', 'bboxHeight'])
-            for i in range(1, shapeLabelFilter.GetNumberOfLabels()+1):
-                writer.writerow([i, shapeLabelFilter.GetRoundness(i),
-                                shapeLabelFilter.GetNumberOfPixels(i), 
-                                shapeLabelFilter.GetBoundingBox(i)[0],
-                                shapeLabelFilter.GetBoundingBox(i)[1],
-                                shapeLabelFilter.GetBoundingBox(i)[2],
-                                shapeLabelFilter.GetBoundingBox(i)[3]])
-        print(os.path.join(csvPath, csvFile))
+  csvFile = os.path.splitext(image)[0] + '.csv'
+  with open(os.path.join(csvPath, csvFile), 'w', newline='') as csvfile:
+      writer = csv.writer(csvfile)
+      writer.writerow(['id', 'roundness', 'pixels', 'bboxX', 'bboxY', 'bboxWidth', 'bboxHeight'])
+      for i in range(1, shapeLabelFilter.GetNumberOfLabels()+1):
+          writer.writerow([i, shapeLabelFilter.GetRoundness(i),
+                          shapeLabelFilter.GetNumberOfPixels(i), 
+                          shapeLabelFilter.GetBoundingBox(i)[0],
+                          shapeLabelFilter.GetBoundingBox(i)[1],
+                          shapeLabelFilter.GetBoundingBox(i)[2],
+                          shapeLabelFilter.GetBoundingBox(i)[3]])
+  print(os.path.join(csvPath, csvFile))
