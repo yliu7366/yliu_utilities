4622e4bb71b635a37364908079e2a88a3da819d4
Yanling Liu <30328162+yliu7366@users.noreply.github.com>
2019-10-10 10:30:42 -0400

    Create SimpleITK_remove_labels_less_than.py
    Using SimpleITK to remove labels less than 4x4=16 pixels in size and save out the filtered labels as binary image.

---
+++ SimpleITK_remove_labels_less_than.py
@@ -0,0 +1,27 @@
+import SimpleITK as sitk
+
+img = sitk.ReadImage('./data/watershed.png')
+
+print(img.GetSize())
+
+labels = sitk.RelabelComponent(sitk.ConnectedComponent(img))
+shapeLabelFilter = sitk.LabelShapeStatisticsImageFilter()
+shapeLabelFilter.Execute(labels)
+
+print(shapeLabelFilter.GetNumberOfLabels())
+
+relabelMap =  { i : 0 for i in shapeLabelFilter.GetLabels() if shapeLabelFilter.GetNumberOfPixels(i) < 16 }
+
+output = sitk.ChangeLabel(labels, changeMap=relabelMap) 
+
+shapeLabelFilter1 = sitk.LabelShapeStatisticsImageFilter()
+shapeLabelFilter1.Execute(output)
+
+print(shapeLabelFilter1.GetNumberOfLabels())
+
+#convert label maps to binary image
+labelImg = (output != 0)
+
+sitk.WriteImage(labelImg * 255, './data/filtered.png')
+
+print("done.")
