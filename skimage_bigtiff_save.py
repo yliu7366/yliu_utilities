2b6181506b032534dbfee9d56d7424224ec6be12
Yanling Liu <30328162+yliu7366@users.noreply.github.com>
2020-05-16 19:21:40 -0400

    Create skimage_bigtiff_save.py

---
+++ skimage_bigtiff_save.py
@@ -0,0 +1,5 @@
+#can not fine an existing example of setting the correct parameters
+#for skimage.imsave saving bigtiff files. So here it goes:
+
+kwargs = {'bigtiff': True}
+imsave(os.path.join(outPath, token+'.tif'), r, check_contrast=False, **kwargs)
