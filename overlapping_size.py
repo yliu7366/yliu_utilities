eb601e21cbd2cacaee8e722d3072db018e82354a
Yanling Liu <30328162+yliu7366@users.noreply.github.com>
2021-04-06 21:31:49 -0400

    Update overlapping_size.py

--- overlapping_size.py
+++ overlapping_size.py
@@ -1,16 +1,16 @@
 #calculate new image size for overlapping patch extraction and stitching
-#use newSize // wndSize for patch extraction
+#use (img_size + padding) // wndSize + 1 for patch extraction
 #img_size: input image size, for example [121, 610, 1280]
 #padding: paddings to the input image, for example, [16, 16, 16]
 #pch_size: patch size, for example [ 64, 128, 128]
 #pch_overlapping: patch overlapping, for example, [16, 16, 16]
 #wndSize: effective patch size, equals patch size minus patch_overlapping * 2
 def overlappingSize(img_size, padding, pch_size, pch_overlapping):
-  newSize = []
+    newSize = []
   
-  for i, p, ps, po in zip(img_size, padding, pch_size, pch_overlapping):
-    wndSize = ps - po*2
-    newSize.append( ((i+p) // wndSize) * wndSize + ps )
+    for i, p, ps, po in zip(img_size, padding, pch_size, pch_overlapping):
+        wndSize = ps - po*2
+        newSize.append( ((i+p) // wndSize) * wndSize + ps * 2 )
     
-  return newSize
+    return newSize
 #end def overlappingSize
