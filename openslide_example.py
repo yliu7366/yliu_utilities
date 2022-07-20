34b6da0ee9eb03e0c4f0f487143471d5b5b18580
Yanling Liu <30328162+yliu7366@users.noreply.github.com>
2022-07-20 10:38:47 -0400

    Update openslide_example.py

--- openslide_example.py
+++ openslide_example.py
@@ -1,5 +1,16 @@
+import PIL
+from PIL import Image
+
+#bypass the size limit
+Image.MAX_IMAGE_PIXELS = 200000*200000
+
 INPUTS = glob.glob(os.path.join(INPUT, '*.svs'))
 
 for w in INPUTS:
   wsi = openslide.OpenSlide(w)
   wsiNpy = np.array(wsi.read_region((0,0), 0, (wsi.dimensions[0], wsi.dimensions[1])))[:,:,:3]
+
+#take out a 256x256 patch from top left (0, 0)
+roi = wsiNpy[0:256, 0:256]
+#take out a 256x256 patch from (256, 0)
+roi = wsiNpy[0:256, 256:512]
